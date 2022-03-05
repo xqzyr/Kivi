676b5ba0c5086b79c292f40eb335db3ab8ff689b
Ashutosh Jaiswal <72222733+xqzyr@users.noreply.github.com>
2022-03-05 21:43:08 +0530

    Created and connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,29 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-app.listen(8800, () => {
-    console.log("Backend server is running!")
-})
+// Loads env vars from .env file into process.env
+require("dotenv").config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, unseUnifiedTopology: true },
+    () => {
+        console.log("Connected to MongoDB");
+    }
+);
+
+const app = express();
+app.listen(3000, () => {
+    console.log("Backend server is running!");
+});
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("Working");
+});
